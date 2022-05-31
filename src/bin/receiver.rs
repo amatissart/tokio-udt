670380c34ce398ccb269f6b93b44ff9da61fc8b6
Adrien Matissart <a@matissart.net>
2022-05-31 11:23:38 +0200

    receiver and sender with mmesg

--- src/bin/receiver.rs
+++ src/bin/receiver.rs
@@ -1,64 +1,101 @@
+use clap::Parser;
+use nix::sys::socket::{recvmmsg, MsgFlags, RecvMmsgData, RecvMsg, SockaddrStorage};
 use socket2::{Domain, Socket, Type};
+use tokio::io::{Error, ErrorKind, Interest};
 use tokio::net::UdpSocket;
 
+use std::io::IoSliceMut;
 use std::net::SocketAddr;
+use std::os::unix::io::AsRawFd;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
-use std::time::{Duration, Instant};
 use std::thread::sleep;
-use std::os::unix::io::AsRawFd;
+use std::time::{Duration, Instant};
 
+#[derive(Parser)]
+struct Args {
+    #[clap(short, default_value_t = 9000)]
+    port: u16,
+}
 
-
-async fn recv(count: Arc<AtomicUsize>) -> tokio::task::JoinHandle<()> {
+async fn recv<'a>(port: u16, count: Arc<AtomicUsize>) {
     let socket = Socket::new(Domain::IPV4, Type::DGRAM, None).unwrap();
-    let address: SocketAddr = "0.0.0.0:9001".parse().unwrap();
+    let address = SocketAddr::new("0.0.0.0".parse().unwrap(), port);
     socket.set_reuse_port(true).unwrap();
-    // socket.set_recv_buffer_size(1_000_000).unwrap();
+    socket.set_recv_buffer_size(1_000_000).unwrap();
+    socket.set_nonblocking(true).unwrap();
     socket.bind(&address.into()).unwrap();
     let socket: UdpSocket = UdpSocket::from_std(socket.into()).unwrap();
 
-    let mut buffer = [0; 2048];
     let mut last = Instant::now();
-    let raw_fd = socket.as_raw_fd();
+
+    let mut local_count = 0;
 
     loop {
-        let (size, _) = socket.recv_from(&mut buffer).await.unwrap();
-        count.fetch_add(1, Ordering::SeqCst);
+        socket.readable().await.unwrap();
+        let npkts = socket
+            .try_io(Interest::READABLE, || {
+                let sock_fd = socket.as_raw_fd();
+                let mut buffers = [[0; 2048]; 100];
+                let mut recv_mesg_data: Vec<RecvMmsgData<_>> = buffers
+                    .iter_mut()
+                    .map(|b| RecvMmsgData {
+                        iov: [IoSliceMut::new(&mut b[..])],
+                        cmsg_buffer: None,
+                    })
+                    .collect();
+                let nptks: Vec<RecvMsg<'_, SockaddrStorage>> =
+                    recvmmsg(sock_fd, &mut recv_mesg_data, MsgFlags::MSG_DONTWAIT, None).map_err(
+                        |err| {
+                            if err == nix::errno::Errno::EWOULDBLOCK {
+                                return Error::new(ErrorKind::WouldBlock, "recvmmsg would block");
+                            }
+                            Error::new(ErrorKind::Other, err)
+                        },
+                    )?;
+                Ok(nptks.len())
+            })
+            .unwrap_or(0);
+
+        // let (size, _) = socket.recv_from(&mut buffer).await.unwrap();
+        local_count += npkts;
+        count.fetch_add(npkts, Ordering::SeqCst);
 
         if last.elapsed() > Duration::new(1, 0) {
             last = Instant::now();
 
-            println!("\t(size is {}) from {}", size, socket.as_raw_fd());
+            println!(
+                "\treceived {} packets via {}",
+                local_count,
+                socket.as_raw_fd()
+            );
         }
     }
 }
 
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
+    let port = args.port;
     let count = Arc::new(AtomicUsize::new(0));
+
+    std::thread::spawn({
+        let count = count.clone();
+        move || loop {
+            sleep(Duration::from_secs(1));
+            println!("Received {} packets", count.load(Ordering::SeqCst));
+        }
+    });
 
     tokio::try_join!(
         tokio::spawn({
             let count = count.clone();
-            async move {
-                loop {
-                    sleep(Duration::from_secs(1));
-                    println!("Received {} packets", count.load(Ordering::SeqCst));
-                }
-            }
+            async move { recv(port, count).await }
         }),
         tokio::spawn({
             let count = count.clone();
-            async move { recv(count).await }
+            async move { recv(port, count).await }
         }),
-        // tokio::spawn({
-        //     let count = count.clone();
-        //     async move { recv(count).await }
-        // }),
-        // tokio::spawn({
-        //     let count = count.clone();
-        //     async move { recv(count).await }
-        // }),
-    ).unwrap();
+    )
+    .unwrap();
 }
--- src/bin/sender.rs
+++ src/bin/sender.rs
@@ -1,28 +1,35 @@
+use clap::Parser;
 use socket2::{Domain, Socket, Type};
+use tokio::io::Interest;
+use tokio::io::{Error, ErrorKind};
 use tokio::net::UdpSocket;
 
 use nix::sys::socket::{sendmmsg, MsgFlags, SendMmsgData, SockaddrIn};
 use std::io::IoSlice;
-use std::net::{SocketAddr, SocketAddrV4};
+use std::net::SocketAddrV4;
 use std::os::unix::io::AsRawFd;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 use std::time::{Duration, Instant};
 
-async fn send(nb_packets: Arc<AtomicUsize>) {
+#[derive(Parser)]
+struct Args {
+    dest_ip: String,
+
+    #[clap(short, default_value_t = 9000)]
+    port: u16,
+}
+
+async fn send(dest: SocketAddrV4, nb_packets: Arc<AtomicUsize>) {
     let socket = Socket::new(Domain::IPV4, Type::DGRAM, None).unwrap();
-    let address: SocketAddr = "0.0.0.0:9000".parse().unwrap();
     socket.set_reuse_port(true).unwrap();
     socket.set_send_buffer_size(1_000_000).unwrap();
-    // println!("SND BUFFER SIZE {}", socket.send_buffer_size().unwrap());
-    socket.bind(&address.into()).unwrap();
+    socket.set_nonblocking(true).unwrap();
     let socket: UdpSocket = UdpSocket::from_std(socket.into()).unwrap();
-    let sock_fd = socket.as_raw_fd();
 
-    let dest: SocketAddrV4 = "127.0.0.1:9001".parse().unwrap();
     let dest: SockaddrIn = dest.into();
 
-    let buffers: Vec<SendMmsgData<_, _, _>> = (0..30)
+    let buffers: Vec<SendMmsgData<_, _, _>> = (0..200)
         .map(|_| SendMmsgData {
             iov: [IoSlice::new(b"Hello World!")],
             cmsgs: &[],
@@ -34,51 +41,62 @@ async fn send(nb_packets: Arc<AtomicUsize>) {
     let mut last = Instant::now();
     let mut packets = 0;
 
-    for _ in 0..100_000 {
+    for _ in 0..50_000 {
         socket.writable().await.unwrap();
-        let npkts = sendmmsg(sock_fd, &buffers, MsgFlags::MSG_DONTWAIT)
-            .unwrap()
-            .into_iter()
-            .filter(|x| *x > 0)
-            .count();
+        let npkts = socket
+            .try_io(Interest::WRITABLE, || {
+                let sock_fd = socket.as_raw_fd();
+                let npkts = sendmmsg(sock_fd, &buffers, MsgFlags::MSG_DONTWAIT)
+                    .map_err(|err| {
+                        if err == nix::errno::Errno::EWOULDBLOCK {
+                            return Error::new(ErrorKind::WouldBlock, "sendmmsg would block");
+                        }
+                        Error::new(ErrorKind::Other, format!("{}", err))
+                    })?
+                    .into_iter()
+                    .filter(|x| *x > 0)
+                    .count();
+                Ok(npkts)
+            })
+            .unwrap_or(0);
 
         packets += npkts;
         nb_packets.fetch_add(npkts, Ordering::SeqCst);
 
         if last.elapsed() > Duration::from_secs(1) {
             last = Instant::now();
-
-            println!("\tSent {} packets to {}", packets, sock_fd);
+            let sock_fd = socket.as_raw_fd();
+            println!("\tSent {} packets via {}", packets, sock_fd);
         }
     }
 }
 
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
+    let dest = SocketAddrV4::new(args.dest_ip.parse().unwrap(), args.port);
+
     let nb_packets = Arc::new(AtomicUsize::new(0));
 
     let spawn = || {
         tokio::spawn({
             let nb_packets = nb_packets.clone();
-            async move { send(nb_packets).await }
+            async move { send(dest, nb_packets).await }
         })
     };
 
+    std::thread::spawn({
+        let nb_packets = nb_packets.clone();
+        move || loop {
+            std::thread::sleep(Duration::from_secs(1));
+            println!("Sent {} packets", nb_packets.load(Ordering::SeqCst));
+        }
+    });
+
     tokio::try_join!(
-        tokio::spawn({
-            let nb_packets = nb_packets.clone();
-            async move {
-                loop {
-                    tokio::time::sleep(Duration::from_secs(1)).await;
-                    println!("Sent {} packets", nb_packets.load(Ordering::SeqCst));
-                }
-            }
-        }),
-        spawn(),
-        spawn(),
-        spawn(),
-        spawn(),
         spawn(),
         spawn(),
     ).unwrap();
+
+    println!("Sent {} packets", nb_packets.load(Ordering::SeqCst));
 }
