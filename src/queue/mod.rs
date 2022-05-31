10517f2493e9bc8a70fa05b7dc1f1c5f64ed0aaf
Adrien Matissart <a@matissart.net>
2022-05-31 16:49:47 +0200

    clean

--- src/queue/mod.rs
+++
@@ -1,9 +0,0 @@
-mod rcv_buffer;
-mod rcv_queue;
-mod snd_buffer;
-mod snd_queue;
-
-pub(crate) use rcv_buffer::RcvBuffer;
-pub(crate) use rcv_queue::UdtRcvQueue;
-pub(crate) use snd_buffer::SndBuffer;
-pub(crate) use snd_queue::UdtSndQueue;
